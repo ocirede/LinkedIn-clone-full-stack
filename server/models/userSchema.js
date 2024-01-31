862d9e1fbd241dc697e2cf20ac6adabdc47127c6
Ibrahim Mansour <Ibrahim0Mansour@gmail.com>
2024-01-31 10:27:16 +0100

    fixing schema

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-username: { type: String, required: true },
+username: { type: String, required: true, unique: true},
 password: { type: String, required: true },
-email: { type: String, required: true },
-  
+email: { type: String, required: true, unique: true},
+image: { type: String },
 },
 {
   timestamps: true,
