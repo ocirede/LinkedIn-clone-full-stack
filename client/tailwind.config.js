71940ff5d295143bfb0ed1f4825bc86bc7325260
Ibrahim Mansour <Ibrahim0Mansour@gmail.com>
2024-01-31 09:30:20 +0100

    the real initial commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'xs': '480px',   // Custom extra small screen size
+        'sm': '640px',   // Small screen size
+        'md': '768px',   // Medium screen size
+        'lg': '1024px',  // Large screen size
+        'xl': '1280px',  // Extra large screen size
+      },
+    },
+  },
+  plugins: [],
+}
+
