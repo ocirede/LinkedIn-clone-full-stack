a75885cf43675b399731a8b520ad61157f1907d1
Ibrahim Mansour <Ibrahim0Mansour@gmail.com>
2024-01-31 12:00:58 +0100

    dividing home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import HomePage from "./pages/HomePage";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <BrowserRouter>
+        {/* <UserContextProvider>
+          <PostContextProvider> */}
+            <Routes>
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/" element={<SignUp />} />
+              <Route path="/home" element={<HomePage />} />
+            </Routes>
+          {/* </PostContextProvider>
+        </UserContextProvider> */}
+      </BrowserRouter>
     </>
-  )
+  );
 }
+export default App;
 
-export default App
+
+
+
+
+
+
+
+
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <>
+    <div className="flex h-screen">
+            <div className="hidden xl:block w-2/6 p-4 bg-gray-200">
+
+            </div>
+            <div className="flex-1 p-4 w-2/6">
+            
+            </div>
+            <div className="hidden xl:block w-2/6 p-4 bg-gray-200 -z-10">
+
+            </div>
+            </div>
+    </>
+  )
+}
