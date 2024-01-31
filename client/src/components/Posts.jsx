5f750cef47e73d06f9563beaded632f0a534901c
KostasBzn <kostasbouzianis@gmail.com>
2024-01-31 23:45:22 +0100

    kostas

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -14,7 +14,6 @@ const Posts = () => {
   } = useContext(PostContext);
   const [commentText, setCommentText] = useState("");
   const [showCommentInput, setShowCommentInput] = useState({});
-  const [postComments, setPostComments] = useState([]);
 
   useEffect(() => {
     if (allPosts) {
