03be1f3a952b1c806456a9c666a23abed121fe9f
Federico <federico.diaferia@gmail.com>
2024-01-31 11:43:33 +0100

    testing with front end

---
+++ server/models/postSchema.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+
+}, {
+    timestamps: true,
+});
+
+const Post = mongoose.model("post", postSchema);
+
+export default Post;
