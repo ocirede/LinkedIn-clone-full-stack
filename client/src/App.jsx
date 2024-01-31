acf1903f41e5133be71d59731505202bf0b98f0e
Mohammad Ali Ashraf <ashraf182003@gmail.com>
2024-01-31 16:05:57 +0100

    1stcommifromnavbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+          {/* <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
