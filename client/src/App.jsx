6cd3a95cf0e7d0ebc2f8935d5b3fe2c9951ef869
Ibrahim Mansour <Ibrahim0Mansour@gmail.com>
2024-01-31 20:38:04 +0100

    commiting changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import HomePage from "./pages/HomePage";
+import UserContextProvider from "./context/UserContextProvider"
+import PostContextProvider from "./context/PostContextProvider"
 function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <UserContextProvider>
-          <PostContextProvider> */}
+        <UserContextProvider>
+          <PostContextProvider>
             <Routes>
               <Route path="/signin" element={<SignIn />} />
               <Route path="/" element={<SignUp />} />
               <Route path="/home" element={<HomePage />} />
             </Routes>
-          {/* </PostContextProvider>
-        </UserContextProvider> */}
+          </PostContextProvider>
+        </UserContextProvider>
       </BrowserRouter>
     </>
   );
