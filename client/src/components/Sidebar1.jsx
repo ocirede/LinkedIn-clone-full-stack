929b98a7d6872447e31d9f0ed8b906fa2bf38d8c
Ibrahim Mansour <Ibrahim0Mansour@gmail.com>
2024-01-31 21:13:58 +0100

    added sidebar elements

---
+++ client/src/components/Sidebar1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Sidebar1() {
+  return (
+    <div className='w-2/3 bg-white h-2/5 rounded-lg'>
+      
+
+    </div>
+  )
+}
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-
+import Sidebar1 from '../components/Sidebar1'
 export default function HomePage() {
   return (
     <>
     <div className="flex h-screen">
-            <div className="hidden xl:block w-2/6 p-4 bg-gray-200">
-
+            <div style={{backgroundColor: "#F4F2EE"}} className=" w-2/6 p-4 flex flex-col items-center">
+            <Sidebar1/>
             </div>
             <div className="flex-1 p-4 w-2/6">
             
             </div>
-            <div className="hidden xl:block w-2/6 p-4 bg-gray-200 -z-10">
+            <div className=" w-2/6 p-4 bg-gray-200 -z-10">
 
             </div>
             </div>
