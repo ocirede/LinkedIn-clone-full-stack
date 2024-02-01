d59dd401ed37ceb460d6364194fb594ecb9846b2
Mohammad Ali Ashraf <ashraf182003@gmail.com>
2024-02-01 12:15:18 +0100

    commit

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Sidebar1 from '../components/Sidebar1'
 import Hero from '../components/Hero'
 import Sidebar3 from '../components/Sidebar3'
 export default function HomePage() {
@@ -10,10 +9,10 @@ export default function HomePage() {
             <Hero/>
             </div>
             <div style={{backgroundColor: "#F4F2EE"}} className=" w-3/5 p-1 flex flex-col items-center">
-            <Hero/>
+            
             </div>
             <div style={{backgroundColor: "#F4F2EE"}} className=" w-1/5 p-2 flex flex-col items-center">
-              <Sidebar1/>
+              <Sidebar3/>
             </div>
       </div>
     </>
